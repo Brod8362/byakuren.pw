a25d97181f675e6350d38bef7767319da4f15219
Brod8362 <brod8362@gmail.com>
2023-02-07 17:36:24 -0600

    Serve static files/render blogs

---
+++ pages/test.md
@@ -0,0 +1,20 @@
+# Test Post
+
+1. Numbered
+2. List
+3. Elements
+4. Number fifteen
+
+- Unordered
+- List
+- Elements
+
+## Double Hash
+
+some content
+
+### Triple Hash
+
+**bold content**
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 extern crate rocket;
 
+use std::fs;
+use comrak::{Arena, ComrakOptions, format_html, parse_document};
 use rocket::{Build, get, Rocket, routes, launch};
+use rocket::fs::FileServer;
 use rocket::http::Status;
-use rocket_dyn_templates::Template;
+use rocket_dyn_templates::{context, Template};
 
 #[get("/page/<doc_name>")]
 fn render_doc(doc_name: String) -> Result<Template, Status> {
-    Err(Status::NotFound)
+    let arena = Arena::new();
+    let content = match fs::read_to_string(format!("pages/{}.md", doc_name)) {
+        Ok(c) => c,
+        Err(_) => return Err(Status::NotFound)
+    };
+    let root = parse_document(&arena, &*content, &ComrakOptions::default());
+    let mut html = vec![];
+    format_html(root, &ComrakOptions::default(), &mut html).unwrap();
+
+    Ok(
+        Template::render("post", context! {
+            rendered_page: String::from_utf8(html).unwrap()
+        })
+    )
 }
 
 #[launch]
 pub async fn rocket() -> Rocket<Build> {
     rocket::build()
         .mount("/", routes![render_doc])
+        .mount("/static", FileServer::from("static"))
         .attach(Template::fairing())
 }
