81a0093e06cc3f040ffcb6d343b3733425c4251c
Brod8362 <brod8362@gmail.com>
2023-02-07 19:52:22 -0600

    Update github link

--- src/util.rs
+++ src/util.rs
@@ -4,7 +4,7 @@ use std::os::unix::fs::MetadataExt;
 pub fn all_pages() -> Vec<(String, i64)> {
     match fs::read_dir("pages") {
         Ok(paths) => {
-            paths.into_iter()
+            let mut posts: Vec<(String, i64)> = paths.into_iter()
                 .filter_map(|f| {
                     let dir = f.unwrap();
                     let mut t = String::from(dir
@@ -24,7 +24,9 @@ pub fn all_pages() -> Vec<(String, i64)> {
                         None
                     }
                 })
-                .collect()
+                .collect();
+                posts.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+                posts
         }
         Err(_) => panic!("can't determine page list")
     }
